9e27757ba2d3da0055c9f642c7c050241924e9f3
Lucy Dobson <lgdobson@willamette.edu>
2025-10-23 15:05:40 -0700

    Still in progress

--- numerical_io/src/main.rs
+++ numerical_io/src/main.rs
@@ -1,4 +1,31 @@
+/*
+ * pub fn u64_to_ix(val: u64) -> ix {
+      return ix {
+          sign: false,
+          vals: vec![val],
+      };
+  }
+  
+  fn add_ix(n1: &ix,n2: &ix) -> ix{
+      //n1 and n2 convert them both to u128
+      let answer : ix = ix::new();
+      /*let mut count : u8 = 2;
+      let mut remainder : u8 = 0;
+  
+      let p1 :u64 = (n1 & 0b11);
+      let p2 :u64 = (n1 >> count) & 0b11; 
+  
+      let s1 :u64 = (n2 & 0b11);
+      let s2 :u64 = (n2 >> count) & 0b11; 
+      */
+  
+  
+      answer
+  }
 
+ *
+ *
+ */
 
 
 
