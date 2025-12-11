8a42c6885e7ca454f0be42eac98442d2e26d845f
Lucy Dobson <lgdobson@willamette.edu>
2025-12-10 23:44:44 -0800

    Fixed syntax, no diff yet

--- Test_Repo/SCM/src/main.rs
+++ Test_Repo/SCM/src/main.rs
@@ -1,12 +1,9 @@
 use std::*;
 use std::fs;
 use std::env;
-use std::path::Path;
 use serde_json::{json, Value};
 use std::collections::HashMap;
 use diffy::create_patch;
-
-
 
 fn inv() -> Vec<String> {
     let entries = fs::read_dir("./")
@@ -25,8 +22,7 @@ fn inv() -> Vec<String> {
         if !file_name.starts_with(".") {
             files.push(file_name)
         }
-    }
-   
+    }   
     files.sort();
     files
 }
@@ -34,13 +30,11 @@ fn inv() -> Vec<String> {
 
 fn main() {
     println!();
-    
     let mut latest_stuff: HashMap<String, String> = HashMap::new();
     let file_names: Vec<String> = inv();
-    //let mut file_data : Vec<String> = Vec::new();
     let args: Vec<String> = env::args().collect();
+    
     dbg!(&args);
-
     println!();
     
     if args.len() < 2 {
@@ -49,19 +43,14 @@ fn main() {
     }
 
     let input = args[1].trim().to_lowercase();
-    
     if input == "help".to_string(){
         println!("List of commands: \n  . commit \n  . revert");
     }
-
 
     else if input.trim() == "commit".to_string(){
         if !fs::exists(".scm").expect("Can't check existence of file does_not_exist.txt") {
             println!("No .scm file found!");
             fs::write(".scm","").expect("Failed to make .scm file");
-            
-           //FIXME this could go directly into a json! call? 
-           // let mut latest_stuff: HashMap<String, String> = HashMap::new();
           
             println!("Files in Directory: ");
             
@@ -70,13 +59,10 @@ fn main() {
                 let contents = fs::read_to_string(&file)
                     .expect("Should have been able to read the file");
                 latest_stuff.insert(file, contents);
-                
             }
-
 
             let commit_stuff = json!({"init": latest_stuff, "diff": "{}"});
             let json_stuff = json!({"latest": latest_stuff, "commit": commit_stuff});
-            //let json_init = json!({"commit": {"init": latest_stuff, "diff": "{}"});
             let pretty_json: String = serde_json::to_string_pretty(&json_stuff).expect("String is not pretty");
             println!("Pretty json : {}", pretty_json);
             fs::write(".scm", pretty_json).expect("Can not write json")
@@ -85,8 +71,6 @@ fn main() {
             println!(".scm file found! ");
             let scm_content: String = fs::read_to_string(".scm").expect("Can't read file to string");
             println!("scm: {}",scm_content);
-           // let scm_json: Value = serde_json::from_str(&scm_content);
-            //let json_data : Value = serde_json::from_str("{\"field\": \"value\"}").expect("failed to convert to json");
             let json_thingy: Value = 
             match fs::read_to_string(".scm") {
 
@@ -105,26 +89,32 @@ fn main() {
                 }
             };
 
-
-
-
             //original == scm_content
             //modified == current file
+            //file name = "file"
             
             //let patch = create_patch(&scm_content, latest_stuff);
             for file in file_names {
+                if(".scm"[file])
                 //let modified: String = commit_stuff[file];
-               
+               // how do you find out 
                 let contents = fs::read_to_string(&file)
                     .expect("Should have been able to read the file");
                 latest_stuff.insert(file, contents);
             }
             
 
-            //read in .scm file and convert to json
             //for each file in directory compare to .scm version
             //diff the two verions
             //append that output into "diff"
+        
+
+            // rotate through all files
+            // see if there are any not in scm
+            // add an x to all files
+            // use diffy to get difference and see if it works
+
+
         }
 
     }
